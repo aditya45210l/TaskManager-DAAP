be0d36bc87a80a349757b266978ac2d856a0202d
aditya45210l <aditya45210l@gmail.com>
2025-06-03 23:35:51 +0530

    initilized next.js into frontend path

--- backend/index.js
+++ backend/index.js
@@ -5,12 +5,21 @@ import errorMiddleware from "./middlewares/error.middleware.js";
 import connectToDatabase from "./database/mongodb.js";
 import { watchAllEvents } from "./listning.js";
 import { PORT } from "./configs/env.js";
+import cors from "cors";
 import { handleTaskCreated } from "./controllers/taskCreated.controller.js";
 
+
 const app = express();
+app.use(cors());
+app.use(express.json());
 
 app.use("/api/v1/user", userRouter);
 app.use("/api/v1/task", taskRouter);
+app.get("/", (req, res) => {
+  res.status(200).json({
+    message: "Welcome to the Task Management API",
+  });
+});
 app.use(errorMiddleware);
 
 app.listen(PORT, async () => {
