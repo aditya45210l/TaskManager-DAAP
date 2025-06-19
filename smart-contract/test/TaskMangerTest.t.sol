690c80bb127bd3e723bd5713e401883358195385
aditya45210l <aditya45210l@gmail.com>
2025-06-19 15:57:45 +0530

    write more tests

--- smart-contract/test/TaskMangerTest.t.sol
+++ smart-contract/test/TaskMangerTest.t.sol
@@ -59,4 +59,71 @@ contract TestTaskManager is Test {
         );
     }
 
+    // checking anyone can calim the tasks only one time;
+    function testOnlyOneUserCanAbleToClaimTask() public createTask {
+        vm.prank(BOB);
+        taskManager.claimTask(0);
+        vm.prank(address(21));
+        vm.expectRevert(TaskManager.TaskManager__TaskAllreadyClaimed.selector);
+        taskManager.claimTask(0);
+    }
+
+    function testOwerNotAbleToCallClaimTask() public createTask {
+        vm.prank(ALICE);
+        vm.expectRevert(TaskManager.TaskManager__OwnerCannotClaimTask.selector);
+        taskManager.claimTask(0);
+    }
+
+    function testSubmitTask() public createTask {
+        vm.prank(BOB);
+        taskManager.claimTask(0);
+        vm.prank(BOB);
+        taskManager.submitTask(0);
+
+        TaskManager.Task memory task = taskManager.getTask(0);
+        assertEq(
+            uint256(taskManager.getTaskStatus(0)),
+            uint256(TaskManager.TaskStatus.Verifing)
+        );
+        assertEq(
+            uint256(taskManager.getTaskStatus(0)),
+            uint256(TaskManager.TaskStatus.Verifing),
+            "Task status mismatch"
+        );
+        assertEq(
+            task.completer,
+            address(0),
+            "Task completer should be address(0)"
+        );
+        assertEq(task.claimer, BOB, "Task claimer mismatch");
+    }
+    function testTaskOnlySubmitByClaimer() public createTask{
+        vm.prank(BOB);
+        taskManager.claimTask(0);
+        vm.expectRevert(TaskManager.TaskManager__onlyTaskClaimerCanCallThisFunc.selector);
+        vm.prank(address(3243));
+        taskManager.submitTask(0);
+
+    }
+
+    function testFullWorkFlowOfContract() public createTask {
+        uint256 bobPrevsBal = BOB.balance;
+        uint256 aliceAfterBal = ALICE.balance;
+        uint256 contractBal = address(taskManager).balance;
+        vm.startPrank(BOB);
+        taskManager.claimTask(0);
+        taskManager.submitTask(0);
+        vm.stopPrank();
+        vm.prank(ALICE);
+        taskManager.verifyTask(0);
+        vm.prank(BOB);
+        taskManager.cliamReward(0);
+        uint256 bobAfterBal = BOB.balance;
+        uint256 contractAfterBal = address(taskManager).balance;
+
+        assertEq(aliceAfterBal, 90 ether);
+        assertEq(contractBal, 10 ether);
+        assertEq(bobAfterBal, bobPrevsBal + 10 ether);
+        assertEq(contractAfterBal, 0);
+    }
 }
